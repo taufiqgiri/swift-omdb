5d01918043b1749caf0d7d14af27479aafc52f11
taufiqgiri <taufiq.ichwanusofa@ist.id>
2024-06-14 18:31:47 +0700

    Initial Commit

---
+++ swift-omdb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-omdb
+//
+//  Created by Taufiq Ichwanusofa on 14/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
